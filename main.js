3832b7ab4ce34ef59b76e90ab0b827f8a7729c8d
wisteria30 <wis30@wisteria30.local>
2019-11-03 22:57:31 +0900

    hoge

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
+
 window.addEventListener("load", function () {
-    this.localStorage.setItem("pc", "0");
+    let cv;
+    if (this.localStorage.getItem("pc") == null) cv = 0;
+    else cv = this.localStorage.getItem("pc");
+    this.localStorage.setItem("pc", String(cv));
     const num = this.document.getElementById("num");
     num.innerHTML = this.localStorage.getItem("pc");
 
     const up = this.document.getElementById("up");
     const down = this.document.getElementById("down");
     up.addEventListener("click", () => {
-        let n = parseInt(this.localStorage.getItem("pc"), 10) + 1;
-        this.localStorage.setItem("pc", String(n));
+        cv = parseInt(this.localStorage.getItem("pc"), 10) + 1;
+        this.localStorage.setItem("pc", String(cv));
         num.innerHTML = this.localStorage.getItem("pc");
     }, false);
     down.addEventListener("click", () => {
-        let n = parseInt(this.localStorage.getItem("pc"), 10) - 1;
-        this.localStorage.setItem("pc", String(n));
+        cv = parseInt(this.localStorage.getItem("pc"), 10) - 1;
+        this.localStorage.setItem("pc", String(cv));
         num.innerHTML = this.localStorage.getItem("pc");
     }, false);
 }, false);
