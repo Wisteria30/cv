e4cccf3abde074cbab47c330292f2285cfcabbef
wisteria30 <wis30@wisteria30.local>
2019-11-04 12:21:04 +0900

    fix smartphone

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+function update(selecter, event, plusMinus) {
+    const pm = plusMinus ? 1 : -1;
+    selecter.addEventListener(event, () => {
+        cv = parseInt(this.localStorage.getItem("pc"), 10) + pm;
+        cv = cv < 0 ? 0 : cv;
+        this.localStorage.setItem("pc", String(cv));
+        num.innerHTML = this.localStorage.getItem("pc");
+    }, false);
+}
+
+function checkSmartPhone(ua) {
+    return (ua.indexOf('iphone') > -1
+        || ua.indexOf('ipad') > -1 || ((ua.indexOf('android') > -1) && (ua.indexOf('mobile') > -1))
+        || ((ua.indexOf('android') > -1) && (ua.indexOf('mobile') == -1)))
+}
+
 
 window.addEventListener("load", function () {
+    const ua = navigator.userAgent.toLowerCase();
+    this.console.log(ua);
     let cv;
     if (this.localStorage.getItem("pc") == null) cv = 0;
     else cv = this.localStorage.getItem("pc");
@@ -9,14 +27,12 @@ window.addEventListener("load", function () {
 
     const up = this.document.getElementById("up");
     const down = this.document.getElementById("down");
-    up.addEventListener("click", () => {
-        cv = parseInt(this.localStorage.getItem("pc"), 10) + 1;
-        this.localStorage.setItem("pc", String(cv));
-        num.innerHTML = this.localStorage.getItem("pc");
-    }, false);
-    down.addEventListener("click", () => {
-        cv = parseInt(this.localStorage.getItem("pc"), 10) - 1;
-        this.localStorage.setItem("pc", String(cv));
-        num.innerHTML = this.localStorage.getItem("pc");
-    }, false);
+
+    if (checkSmartPhone(ua)) {
+        update(up, "touchend", true);
+        update(down, "touchend", false);
+    } else {
+        update(up, "click", true);
+        update(down, "click", false);
+    }
 }, false);
